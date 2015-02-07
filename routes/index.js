3e23ec7188f13dcfed37061bd3958d34bfbd4f72
Munira <Munira@Muniras-MacBook-Pro.local>
2015-02-07 19:45:11 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'PayGo' ,"age":18});
+});
+
+
+module.exports = router;
